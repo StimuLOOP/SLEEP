a8fc9b47826f34e8d53dc15666e38ffd22a14ff6
tosoo-dev <lauraferster@gmail.com>
2022-11-28 10:42:38 +0100

    [NEW] Template for functions and scripts including header information and license

---
+++ templates/coding/function_template.m
@@ -0,0 +1,63 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% function [output1,output2] = function_name(input1,input2,input3)
+%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Toolboxes required: 
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+%
+% Authors with email: 
+% Date of creation:
+% Version:
+% Validation date/version:
+% Validated by: 
+% Copyright: (c) YEAR.  YOUR NAME, INSTITUTION. All rights reserved
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%------------- BEGIN CODE --------------
+
+%Enter your executable matlab commands here
+
+%------------- END OF CODE --------------
+
+%%%% ---------------------------- LICENCE ---------------------------------%%%%   
+%    (c) 2020-2023. StimuLOOP, All rights reserved.
+%
+%  * Redistribution in source and binary forms, with or without modification, 
+%    are not permitted without previous written approval of the copyright owner.
+%
+%  * Modifications of source code for use must retain the above copyright 
+%    notice, this list of conditions and the following disclaimer.
+%
+%    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   
+%    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+%    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+%    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS 
+%    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+%    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+%    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+%    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+%    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+%    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+%    THE POSSIBILITY OF SUCH DAMAGE.
